bde4b803fd3ff3e35473b98c431b1680fe70ce7d
Sudhan <Sdahake9382@conestogac.on.ca>
2022-11-02 22:17:00 -0400

    The subtract function is initialised and also the main function is updated accordingly.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,9 @@ void main() {
 	{
 	case 1:
 		add();
+
+	case 2:
+		subtract();
 	}
 
 }
@@ -44,4 +48,20 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+
+	double num1;
+	double num2;
+
+	printf("Enter the first value: ");
+	scanf_s("%lf", &num1);
+
+	printf("Enter the second value: ");
+	scanf_s("%lf", &num2);
+
+	double result = num1 - num2;
+
+	printf("%lf - %lf = %lf", num1, num2, result);
 }
